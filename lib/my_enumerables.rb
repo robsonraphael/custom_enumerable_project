7a2bbb264ac7a0103e8c28306485da31abbbeb35
Robson Raphael <robsonraphael2001@gmail.com>
2022-10-02 23:54:07 -0300

    First commit

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -6,6 +6,33 @@ end
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
+
 class Array
   # Define my_each here
+
+  def my_each
+    return to_enum(:my_each) unless block_given? # se não usar bloco vai retonar o metodo.
+
+    for element in self do
+      yield element # seria como o metodos gets, permitindo ao usuario escolher o metodo
+    end
+
+    self
+  end
+
+  def my_each_with_index(*args)
+    return to_enum(:my_each_with_index, *args) unless block_given?
+
+    i = 0
+    for element in self do
+      yield element, i
+      i += 1
+    end
+    self
+  end
+end
+
+meu_array = %w[caju castanha abacate banana melancia]
+meu_array.my_each_with_index do |element, indice|
+  puts element, indice
 end
